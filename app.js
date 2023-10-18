e0b58288e2d06427cbf4fd67aadf09894431dfa6
Dzikri Syairozi <85748069+dzikrisyairozi@users.noreply.github.com>
2023-10-18 08:29:36 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    // Fetching data from the API using AJAX
+    $.ajax({
+        url: 'https://it-its.id/api/movies',
+        method: 'GET',
+        success: function (data) {
+            displayMovies(data);
+        },
+        error: function (error) {
+            console.error("Error fetching data: ", error);
+        }
+    });
+
+    // Function to display movies
+    function displayMovies(movies) {
+        let output = '';
+
+        $.each(movies, function (key, movie) {
+            output += `
+            <div class="col-lg-6 col-12 mb-4">
+                <div class="card shadow p-3 bg-white rounded">
+                    <div class="card-body ">
+                        <h5 class="card-title">${movie.Title}</h5>
+                        <p class="card-text">${movie.Plot}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+
+        $('#moviesList').html(output);
+    }
+});
